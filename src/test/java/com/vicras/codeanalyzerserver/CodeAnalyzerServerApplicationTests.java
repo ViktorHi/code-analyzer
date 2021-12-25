2b85bd06d153235f28bb6fa7e53a63422093de65
vicras <viktor.graskov@x5.ru>
2021-12-25 14:36:41 +0300

    format: test

---
+++ src/test/java/com/vicras/codeanalyzerserver/CodeAnalyzerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicras.codeanalyzerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAnalyzerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
